1b2aa9e5229355353f3e74278e3ea55b4393cefe
enriman2 <158837446+enriman2@users.noreply.github.com>
2024-02-18 18:03:33 +0100

    Update day 2

--- testgit.py
+++ testgit.py
@@ -5,12 +5,21 @@ import os
 load_dotenv()
 client = OpenAI(api_key = os.environ["APIKEY"])
 
-conversation = [{"role": "system", "content": "This is a story: thee are two people on a submarine, and the submarine is going to sink, so they are going to drown soon, but there is a gun with 1 bullet left, so they played a chess game, and the one who wins gets to suicide. So they play the game, one of them wins, and he shoots himself, commiting suicide, so he doesn't drown, he commits suicide. The other one drowns and dies too."},{"role": "system", "content": "The player is going to ask a yes/no question about the story, and you must reason your answer and justify it and then answer Yes or No"}]
+#Put your story here
+story = "Her husband was a magician that was doing a gun trick with an apple on her head and his eyes closed. He didn't realize that she was wearing high heals so he didn't calculate the height accurately.... so he killed her."
+
+conversation = [
+    {"role": "system", "content": "You are going to help the player guess the entierety of the story and you are designed to output JSON"},
+    {"role": "system", "content": "This is a story: " + story},
+    {"role": "system", "content": "The player is going to ask a yes/no question about the story, and you must answer this way: you reason your answer, and then you say Yes or No."},
+    {"role": "system", "content": "If the story is that someone killed his cat, then if the question is 'Did a human die?', your answer should be: {'reasoning': 'Someone killed his cat, so no human died in this story', 'relevance': True, 'yes_or_no_question': True, 'answer': 'No'}. If the question is 'Did the cat die?', then your answer should be: {'reasoning': 'Someone killed his cat, so the cat died', 'relevance': True, 'yes_or_no_question': True, 'answer': 'Yes'}. If the question is 'Did the cat kill the human?', your answer should be: {'reasoning': 'Someone killed his cat, so the cat didn't kill the human', 'relevance': True, 'yes_or_no_question': True, 'answer': 'No'}."}
+]
 
 while True:
     conversation.append({"role": "user", "content": input()})
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-0125",
+        response_format={ "type": "json_object" },
         messages=conversation
     )
     print(response.choices[0].message.content)
